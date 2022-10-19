13c1bc2d501bc284225ca78a1dca7d9abcf99f17
DreamersBoy-Avi <111137283+DreamersBoy-Avi@users.noreply.github.com>
2022-10-19 16:35:02 +0530

    Add files via upload

---
+++ Js Projects/Countdown.js
@@ -0,0 +1,22 @@
+const days = document.querySelector("#days");
+const Hours = document.querySelector("#Hours");
+const Minutes = document.querySelector("#Minutes");
+const Seconds = document.querySelector("#Seconds");
+
+
+function nY() {
+    const currentYear = new Date().getFullYear();
+const newYear = new Date(`January 1 ${currentYear+1} 00:00:00`);
+const currentDate = new Date();
+const diff = newYear-currentDate;
+const d = Math.floor(diff/1000/60/60/24);
+const h = Math.floor((diff/1000/60/60/24)%24);
+const m = Math.floor((diff/1000/60/60/24)%60);
+const s = Math.floor((diff/1000)%60);
+days.innerHTML=d<10?"0"+d:d;
+Hours.innerHTML=h<10?"0"+h:h;
+Minutes.innerHTML=m<10?"0"+m:m;
+Seconds.innerHTML=s<10?"0"+s:s;
+//console.log(d+" "+h+" "+m+" "+s);
+}
+setInterval(nY,1000);
